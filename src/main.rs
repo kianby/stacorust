f2e4a8f7c8f3183b4680b2b00b4a3d7b6514434a
Yax <kianby@madyanne.fr>
2020-01-19 11:58:08 +0100

    toml

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use ini::Ini;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use toml;
+use serde::Deserialize;
+//use serde::serde_derive::Deserialize;
+//use serde::{Serialize, Deserialize};
 
-fn main() {
-    let i = Ini::load_from_file("config.ini").unwrap();
-    for (sec, prop) in i.iter() {
-        println!("Section: {:?}", *sec);
-        for (k, v) in prop.iter() {
-            println!("  {}:{}", *k, *v);
-        }
-    }
+#[derive(Deserialize)]
+struct Config {
+    lang: String,
+    db_url: String
+}
+
+fn main() -> io::Result<()> {
+
+    let mut f = File::open("configtest.toml")?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+    let config: Config = toml::from_str(&buffer).unwrap();
+    println!("lang = {}", config.lang);
+    println!("db url = {}", config.db_url);
+    Ok(())
 }
